715ee71dccdfd2eb5eee0905d2aafab74c550a9d
Kiril Chobanov <62597135+kiril2000@users.noreply.github.com>
2020-04-05 16:23:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace min_max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            for (int i = 0; i < 7; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num > max)
+                {
+                    max = num;
+                }
+
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+
+            Console.WriteLine($"Max: {max} Min: {min}");
+        }
+    }
+}
